9059b703a82a0965c23af888f2c68355ac4c4a9c
marcosgregoret5-wq <marcosgregoret5@gmail.com>
2026-05-28 22:44:03 -0300

    Update send_sales.py

--- send_sales.py
+++ send_sales.py
@@ -1,7 +1,23 @@
 import requests
+import os
+
+WEBHOOK_URL = os.environ["DISCORD_WEBHOOK"]
 
 url = "https://script.google.com/macros/s/AKfycbxqlNW0mNo7FsGo0hR2_2jwJ_WAxC1HiJoKB92Sfupv_1llL1vz04DKRivr-vxPtpQwvQ/exec"
 
 data = requests.get(url).json()
 
-print(data[:3])
+mensaje = "🎮 **OFERTAS SEMANALES DE LEAGUE OF LEGENDS**\n\n"
+
+for skin in data:
+    mensaje += f"🔥 {skin['skin'].title()} - {skin['discount']}% OFF ({skin['price']} RP)\n"
+
+# Discord tiene límite de 2000 caracteres
+mensaje = mensaje[:1900]
+
+requests.post(
+    WEBHOOK_URL,
+    json={"content": mensaje}
+)
+
+print("Mensaje enviado")
