98d19cfa7a726fe9058de0f3cea911e26420af78
marcosgregoret5-wq <marcosgregoret5@gmail.com>
2026-05-28 22:39:36 -0300

    Update send_sales.py

--- send_sales.py
+++ send_sales.py
@@ -1,18 +1,5 @@
 import requests
-import os
 
-WEBHOOK_URL = os.environ["DISCORD_WEBHOOK"]
+html = requests.get("https://lolskinsale.com/").text
 
-mensaje = """
-🎮 OFERTAS SEMANALES DE LEAGUE OF LEGENDS
-
-Revisá las ofertas actuales:
-https://lolskinsale.com/
-
-🔥 Se actualizaron las skins en descuento.
-"""
-
-requests.post(
-    WEBHOOK_URL,
-    json={"content": mensaje}
-)
+print(html[:5000])
