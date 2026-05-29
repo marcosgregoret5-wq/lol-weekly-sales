c3a314a0cbcb97db0c789fa89ad159b5af29ec8a
marcosgregoret5-wq <marcosgregoret5@gmail.com>
2026-05-28 23:05:46 -0300

    Update send_sales.py

--- send_sales.py
+++ send_sales.py
@@ -4,4 +4,8 @@ version = requests.get(
     "https://ddragon.leagueoflegends.com/api/versions.json"
 ).json()[0]
 
-print(version)
+url = f"https://ddragon.leagueoflegends.com/cdn/{version}/data/en_US/championFull.json"
+
+data = requests.get(url).json()
+
+print(list(data["data"].keys())[:20])
